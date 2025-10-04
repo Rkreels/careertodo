5867d18fbbc62d25459bed4613f3105fc06b3405
mycampushub <mycampuslifestyle@gmail.com>
2025-10-04 10:05:37 +0600

    card hight fix

--- components/RoleCardsMarquee.tsx
+++ components/RoleCardsMarquee.tsx
@@ -127,7 +127,7 @@ export function RoleCardsMarquee() {
       <div className="relative w-full h-full max-w-4xl sm:max-w-6xl mx-auto flex gap-4 sm:gap-8 justify-center">
         
         {/* First column - Bottom to Top */}
-        <div className="relative w-64 h-80 sm:h-96 overflow-hidden">
+        <div className="relative w-64 h-80 sm:h-96 lg:w-110 lg:h-64 overflow-hidden">
           {/* Desktop: No shadows, Mobile: No shadows */}
           
           <motion.div
@@ -158,7 +158,7 @@ export function RoleCardsMarquee() {
         </div>
 
         {/* Second column - Top to Bottom */}
-        <div className="relative w-64 h-80 sm:h-96 overflow-hidden hidden lg:block">
+        <div className="relative w-64 h-80 sm:h-96 lg:w-110 lg:h-64 overflow-hidden hidden lg:block">
           {/* Desktop: No shadows */}
           
           <motion.div
